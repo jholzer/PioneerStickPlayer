3008e330a15ac6ea8a5c01fec245674a4e27b587
Jürgen Holzer <juergen.holzer@evopro-ag.de>
2018-07-19 09:11:41 +0200

    Empty Project structure

---
+++ PioneerStickPlayer.DataFileReader.Debug/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PioneerStickPlayer.DataFileReader.Debug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PioneerStickPlayer.DataFileReader.Debug")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fac2f37-a8a7-44e2-8958-d8649886ddf5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PioneerStickPlayer.Interfaces/Enum/PageType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace PioneerStickPlayer.Interfaces.Enum
+{
+    public enum PageType
+    {
+        BlockTracks = 0,
+        BlockGenres = 1,
+        BlockArtists = 2,
+        BlockAlbums = 3,
+        BlockLabels = 4,
+        BlockKeys = 5,
+        BlockColors = 6,
+        BlockPlaylists = 7,
+        BlockPlaylistMap = 8,
+        BlockUnknown4 = 9,
+        BlockUnknown5 = 10,
+        BlockUnknown6 = 11,
+        BlockUnknown7 = 12,
+        BlockArtwork = 13,
+        BlockUnknown8 = 14,
+        BlockUnknown9 = 15,
+        BlockColumns = 16,
+        BlockUnknown1 = 17,
+        BlockUnknown2 = 18,
+        BlockSyncHistory = 19
+    }
+}
